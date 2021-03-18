34f0f4fd530fd9c6a68f6f6572161850bdcd925d
miloszKubiak <milypan69@gmail.com>
2021-03-18 21:47:01 +0100

    Added and styled side panel to change colors

--- main.js
+++ main.js
@@ -6,9 +6,17 @@ const historyBtn = document.querySelector('.history');
 const stopwatch = document.querySelector('.stopwatch');
 const time = document.querySelector('.time');
 const timeList = document.querySelector('.time-list');
-const infoBtn = document.querySelector('.info');
+const infoBtn = document.querySelector('.fa-question');
 const modalShadow = document.querySelector('.modal-shadow');
 const closeModalBtn = document.querySelector('.close');
+
+const colorBtn = document.querySelector('.fa-paint-brush');
+const colorPanel = document.querySelector('.colors');
+const colorOne = document.querySelector('.one');
+const colorTwo = document.querySelector('.two');
+const colorThree = document.querySelector('.three');
+let root = document.documentElement;
+
 let countTime;
 let minutes = 0;
 let seconds = 0;
@@ -92,3 +100,22 @@ historyBtn.addEventListener('click', showHistory);
 infoBtn.addEventListener('click', showModal);
 closeModalBtn.addEventListener('click', showModal);
 window.addEventListener('click', e => e.target === modalShadow ? showModal() : false);
+
+colorBtn.addEventListener('click', () => {
+    colorPanel.classList.toggle('show-colors');
+});
+
+colorOne.addEventListener('click', () => {
+    root.style.setProperty('--first-color', 'rgb(250, 20, 6)');
+    root.style.setProperty('--hover-color', 'rgb(209, 33, 24)');
+});
+
+colorTwo.addEventListener('click', () => {
+    root.style.setProperty('--first-color', 'rgb(6, 173, 250)');
+    root.style.setProperty('--hover-color', 'rgb(28, 145, 199)');
+});
+
+colorThree.addEventListener('click', () => {
+    root.style.setProperty('--first-color', 'rgb(0, 255, 42)');
+    root.style.setProperty('--hover-color', 'rgb(28, 209, 58)');
+});
